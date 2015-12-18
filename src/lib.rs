fbbe86fdaeb6d2b076d6cc24403e14df3621e5fa
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2015-12-18 11:32:33 +0100

    rustup

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #![feature(plugin_registrar, box_syntax, rustc_private)]
 #[macro_use] extern crate rustc;
+extern crate rustc_plugin;
+extern crate rustc_front;
 extern crate syntax;
 
-use rustc::lint::{LintPass, LintArray, Context};
+use rustc::lint::{LintPass, LateLintPass, LintArray, LateContext, LintContext};
 use syntax::attr::contains_name;
-use syntax::ast;
 use rustc::middle::const_eval::eval_const_expr;
-use rustc::middle::const_eval::const_val::*;
-use rustc::plugin::registry::Registry;
+use rustc::middle::const_eval::ConstVal::*;
+use rustc_plugin::registry::Registry;
+use rustc_front::hir;
 
 declare_lint!(STATIC_ASSERT, Deny,
               "check compile-time information");
@@ -18,34 +20,31 @@ impl LintPass for StaticAssertPass {
     fn get_lints(&self) -> LintArray {
         lint_array!(STATIC_ASSERT)
     }
+}
 
-    fn check_item(&mut self, cx: &Context, it: &ast::Item) {
+impl LateLintPass for StaticAssertPass {
+    fn check_item(&mut self, cx: &LateContext, it: &hir::Item) {
         if !contains_name(&it.attrs, "static_assert_") {
             return;
         }
-        let evaluated = match it.node {
-            ast::ItemConst(_, ref expr) => eval_const_expr(&cx.tcx, expr),
-            _ => return,
+        let evaluated = if let hir::ItemConst(_, ref expr) = it.node {
+            eval_const_expr(&cx.tcx, expr)
+        } else {
+            return
         };
         match evaluated {
-            const_bool(true) => return,
+            Bool(true) => {},
+            Bool(false) => cx.span_lint(STATIC_ASSERT, it.span, "static assertion failed"),
             // FIXME: https://github.com/rust-lang/rust/issues/25307
-            const_int(i) if i == 1 => return,
-            const_uint(_) => cx.sess().span_err(it.span, "static assertion on uint"),
-            const_float(_) => cx.sess().span_err(it.span, "static assertion on float"),
-            const_str(_) => cx.sess().span_err(it.span, "static assertion on str"),
-            const_binary(_) => cx.sess().span_err(it.span, "static assertion on binary"),
-            Struct(_) => cx.sess().span_err(it.span, "static assertion on struct"),
-            Tuple(_) => cx.sess().span_err(it.span, "static assertion on tuple"),
-            _ => {}
+            Int(i) if i == 1 => {},
+            _ => cx.sess().span_err(it.span, &format!("static assertion on {:?}", evaluated)),
         }
-        cx.span_lint(STATIC_ASSERT, it.span, "static assertion failed");
     }
 }
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_lint_pass(box StaticAssertPass); // as LintPassObject
+    reg.register_late_lint_pass(box StaticAssertPass); // as LintPassObject
 }
 
 #[macro_export]
